e34a98b672921392aa6778c1939e4033f6a7618e
yundu <yoon12ju@gmail.com>
2021-12-26 01:09:43 +0900

    화면 구성(mustache) 및 CUD 기능 구현

--- src/main/java/com/yun/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/yun/book/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.yun.book.springboot.service.posts;
 
 import com.yun.book.springboot.domain.posts.Posts;
 import com.yun.book.springboot.domain.posts.PostsRepository;
+import com.yun.book.springboot.web.dto.PostsListResponseDto;
 import com.yun.book.springboot.web.dto.PostsResponseDto;
 import com.yun.book.springboot.web.dto.PostsSaveRequestDto;
 import com.yun.book.springboot.web.dto.PostsUpdateRequestDto;
-import javafx.geometry.Pos;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -16,11 +19,42 @@ public class PostsService {
 
     private final PostsRepository postsRepository;
 
+    /**
+     * 글 목록 조회
+     * @return
+     */
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(posts -> new PostsListResponseDto(posts)).collect(Collectors.toList());
+    }
+
+    /**
+     * 게시글 상세 보기
+     * @param id
+     * @return
+     */
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+
+    /**
+     * 게시글 저장
+     * @param requestDto
+     * @return
+     */
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
+    /**
+     * 게시글 수정
+     * @param id
+     * @param requestDto
+     * @return
+     */
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto){
         Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
@@ -30,9 +64,14 @@ public class PostsService {
         return id;
     }
 
-    public PostsResponseDto findById(Long id){
-        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+    /**
+     * 게시글 삭제
+     * @param id
+     */
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
-        return new PostsResponseDto(entity);
+        postsRepository.delete(posts);
     }
 }
